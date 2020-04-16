aed5536b6d7d5af6c4d149d98e11f95841d6ca8b
SERGIOBOSTON <58717846+SERGIOBOSTON@users.noreply.github.com>
2020-04-15 19:54:40 -0500

    Actualización del trabajo

---
+++ Proy_Moneda/Capa_Diseño/FrmPrincipal.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Capa_Diseño
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.TsmCerrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.BtnVinculacion = new System.Windows.Forms.Button();
+            this.BtnRecargas = new System.Windows.Forms.Button();
+            this.BtnEmpleados = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TsmCerrar});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 50;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // TsmCerrar
+            // 
+            this.TsmCerrar.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.TsmCerrar.BackColor = System.Drawing.Color.Red;
+            this.TsmCerrar.ForeColor = System.Drawing.SystemColors.Window;
+            this.TsmCerrar.Name = "TsmCerrar";
+            this.TsmCerrar.Size = new System.Drawing.Size(26, 20);
+            this.TsmCerrar.Text = "X";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(0, 20);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(6, 366);
+            this.textBox2.TabIndex = 49;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(0, 386);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 6);
+            this.textBox1.TabIndex = 48;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(-2, 24);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(797, 6);
+            this.textBox6.TabIndex = 47;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(2)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Location = new System.Drawing.Point(794, 20);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(6, 366);
+            this.textBox3.TabIndex = 46;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(533, 232);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(141, 109);
+            this.button6.TabIndex = 45;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(329, 232);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 109);
+            this.button5.TabIndex = 44;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(121, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 109);
+            this.button4.TabIndex = 43;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // BtnVinculacion
+            // 
+            this.BtnVinculacion.Location = new System.Drawing.Point(533, 77);
+            this.BtnVinculacion.Name = "BtnVinculacion";
+            this.BtnVinculacion.Size = new System.Drawing.Size(141, 109);
+            this.BtnVinculacion.TabIndex = 42;
+            this.BtnVinculacion.Text = "Vinculaciones";
+            this.BtnVinculacion.UseVisualStyleBackColor = true;
+            // 
+            // BtnRecargas
+            // 
+            this.BtnRecargas.Location = new System.Drawing.Point(329, 77);
+            this.BtnRecargas.Name = "BtnRecargas";
+            this.BtnRecargas.Size = new System.Drawing.Size(141, 109);
+            this.BtnRecargas.TabIndex = 41;
+            this.BtnRecargas.Text = "Recargas";
+            this.BtnRecargas.UseVisualStyleBackColor = true;
+            // 
+            // BtnEmpleados
+            // 
+            this.BtnEmpleados.Location = new System.Drawing.Point(121, 77);
+            this.BtnEmpleados.Name = "BtnEmpleados";
+            this.BtnEmpleados.Size = new System.Drawing.Size(141, 109);
+            this.BtnEmpleados.TabIndex = 40;
+            this.BtnEmpleados.Text = "Registro\r\nEmpleados";
+            this.BtnEmpleados.UseVisualStyleBackColor = true;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 391);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.BtnVinculacion);
+            this.Controls.Add(this.BtnRecargas);
+            this.Controls.Add(this.BtnEmpleados);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmPrincipal";
+            this.Text = "FrmPrincipal";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TsmCerrar;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button BtnVinculacion;
+        private System.Windows.Forms.Button BtnRecargas;
+        private System.Windows.Forms.Button BtnEmpleados;
+    }
+}
