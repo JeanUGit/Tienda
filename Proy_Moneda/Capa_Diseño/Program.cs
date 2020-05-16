b702fc6dd135239f82097e5195c25a2c047e06a8
SERGIOBOSTON <58717846+SERGIOBOSTON@users.noreply.github.com>
2020-05-16 09:14:06 -0500

    Imagen para login

--- Proy_Moneda/Capa_Diseño/Program.cs
+++ Proy_Moneda/Capa_Diseño/Program.cs
@@ -16,7 +16,7 @@ namespace Capa_Diseño
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu_Principal());
+            Application.Run(new FrmLogin());
         }
     }
 }
