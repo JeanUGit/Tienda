3376ec05e36c5466e09d32e56380620671b7f994
Jean Carlos <jeancarlosquejadatoro@gmail.com>
2020-06-24 22:16:43 -0500

    Creación de la version2 del proyecto y de la correción del trabajo de Formularios de ventas

---
+++ Proy_Moneda/Capa_Conexion/ClsBuscarProductoXnombre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Capa_Datos
+{
+    public  class ClsBuscarProductoXnombre
+    {
+        private String nombre;
+        private DataTable tabla = new DataTable();
+        protected void Fnt_buscarProducto()
+        {
+            CLConexion conn = new CLConexion();
+            SqlCommand com = new SqlCommand("SP_BuscarProductoxNombre", conn.connection);
+            com.CommandType = CommandType.StoredProcedure;
+            com.Parameters.AddWithValue("@nombre", nombre);
+            conn.connection.Open();
+            SqlDataAdapter data = new SqlDataAdapter(com);
+            data.Fill(tabla);
+            conn.connection.Close();
+
+        }
+
+        public void Fnt_BuscarXNombre(String name) { nombre = name; Fnt_buscarProducto(); }
+
+        public DataTable GetTable() { return tabla; }
+    }
+}
